d43d71b2706fb6dfddebb2aa144cc7cf97ecf75d
anandchouhan50 <anandchouhan50@gmail.com>
2022-02-26 20:29:09 +0530

    colour of 'brands' is changed and new textgeometry added for ampersand

--- js/BRANDS&.js
+++ js/BRANDS&.js
@@ -1,3 +1,5 @@
+// import { OBJLoader } from './jsm/loaders/OBJLoader.js';
+
 const scene = new THREE.Scene();
 // scene.background = new THREE.TextureLoader().load( "img/background.png " );
 
@@ -19,13 +21,13 @@ const genrateLogoLine = () =>
 
 
     const geometry = new THREE.SphereGeometry( 10000000, 500, 500 );
-    const texture = new THREE.TextureLoader().load( 'img/red.png' );
+    const texture = new THREE.TextureLoader().load( 'img/Background-color image 1080p.png' );
     const material = new THREE.MeshBasicMaterial( { color: 0xffffff, map : texture ,side : THREE.DoubleSide} );
     const sphere = new THREE.Mesh( geometry, material );
     sphere.rotation.y = 2
     sphere.rotation.x = 1
     
-    scene.add( sphere );
+    // scene.add( sphere ); 
 
 
 
@@ -58,11 +60,11 @@ const genrateLogoLine = () =>
     
     //texture
     const tradeTextureLoader = new THREE.TextureLoader()
-    const tradeParticalTexture = tradeTextureLoader.load('img/ampersand.jpg')
+    const tradeParticalTexture = tradeTextureLoader.load('img/ampersand.jpeg')
     
     //Material
     const tradeCoinMaterial = new THREE.PointsMaterial();
-    tradeCoinMaterial.size = 3;
+    tradeCoinMaterial.size = 6;
     // tradeCoinMaterial.sizeAttenuation = true;
     // tradeCoinMaterial.color = new THREE.Color(0x00B0DE)
     tradeCoinMaterial.color = new THREE.Color("white")
@@ -116,18 +118,18 @@ const bitcoinGeometry = new THREE.BufferGeometry();
 
 const loader = new THREE.FontLoader();
 
-loader.load( 'js/gentilis_regular.typeface.json', function ( font ) {
+loader.load( 'js/perfect_ampersand.json', function ( font ) {
 
 	const Geometry = new THREE.TextGeometry( '&', {
 		font: font,
-		size: 4.3,
-		height: 0.8,
-		curveSegments: 50,
-		bevelEnabled: false,
+		size: 4,
+		height: 0.3,
+		curveSegments: 500,
+		bevelEnabled: true,
 		bevelThickness: 0.4,
-		bevelSize: 0.1,
-		bevelOffset: 0.2,
-		bevelSegments: 50
+		bevelSize: 0.05,
+		bevelOffset: 0.1,
+		bevelSegments: 5
 	} );
 
     const Material = new THREE.MeshPhongMaterial();
@@ -146,6 +148,7 @@ const count = 200000;
 
 const positions = new Float32Array(count*3);
 const colors = new Float32Array(count*3);
+const rotation = new Float32Array(count*3);
 
 for(let i = 0 ; i < count*3 ; i++ ){
 
@@ -157,18 +160,34 @@ for(let i = 0 ; i < count*3 ; i++ ){
         positions[i3+0] = (Math.random())
         positions[i3+1] = (Math.random()*5)-2.5
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 255
+        colors[i3+2] = 255 
     }else if(i<6000){
             positions[i3+0] = (Math.random()*2.5)+1
             positions[i3+1] = (Math.random())-2.5
             positions[i3+2] = (Math.random())
+
+            colors[i3+0] = 255
+            colors[i3+1] = 255
+            colors[i3+2] = 255 
         }else if(i<8000){
             positions[i3+0] = (Math.random())+2.5
             positions[i3+1] = (Math.random()*2.5)-1.5
             positions[i3+2] = (Math.random())
+
+            colors[i3+0] = 255
+            colors[i3+1] = 255
+            colors[i3+2] = 255 
         }else if(i<8500){
             positions[i3+0] = (Math.random())+1.5
             positions[i3+1] = (Math.random())
             positions[i3+2] = (Math.random())
+
+            colors[i3+0] = 255
+            colors[i3+1] = 255
+            colors[i3+2] = 255 
         }
 
     //r shape    
@@ -176,10 +195,18 @@ for(let i = 0 ; i < count*3 ; i++ ){
         positions[i3+0] = (Math.random())+4.5
         positions[i3+1] = (Math.random()*3.5)-2.5
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 255
+        colors[i3+2] = 255 
     }else if(i<11000){
         positions[i3+0] = (Math.random())+5.5
         positions[i3+1] = (Math.random())
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 255
+        colors[i3+2] = 255 
     }
 
     //a shape
@@ -187,18 +214,34 @@ for(let i = 0 ; i < count*3 ; i++ ){
         positions[i3+0] = (Math.random())+7.5
         positions[i3+1] = (Math.random()*3.5)-2.5
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 0
+        colors[i3+2] = 0   
     }else if(i<15000){
         positions[i3+0] = (Math.random()*2.5)+8.5
         positions[i3+1] = (Math.random())
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 0
+        colors[i3+2] = 0 
     }else if(i<17000){
         positions[i3+0] = (Math.random())+10
         positions[i3+1] = (Math.random()*2.5)-2.5
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 0
+        colors[i3+2] = 0 
     }else if(i<17500){
         positions[i3+0] = (Math.random())+8.5
         positions[i3+1] = (Math.random())-2.5
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 0
+        colors[i3+2] = 0 
     }
 
     //n shape
@@ -206,14 +249,26 @@ for(let i = 0 ; i < count*3 ; i++ ){
         positions[i3+0] = (Math.random())+12
         positions[i3+1] = (Math.random()*3.5)-2.5
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 0
+        colors[i3+2] = 0 
     }else if(i<21500){
         positions[i3+0] = (Math.random()*2.5)+13
         positions[i3+1] = (Math.random())
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 0
+        colors[i3+2] = 0 
     }else if(i<23500){
         positions[i3+0] = (Math.random())+14.5
         positions[i3+1] = (Math.random()*2.5)-2.5
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 0
+        colors[i3+2] = 0 
     }
 
     //d shape
@@ -221,18 +276,34 @@ for(let i = 0 ; i < count*3 ; i++ ){
         positions[i3+0] = (Math.random())+19
         positions[i3+1] = (Math.random()*5)-2.5
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 0
+        colors[i3+2] = 0 
     }else if(i<29500){
             positions[i3+0] = (Math.random()*2.5)+16.5
             positions[i3+1] = (Math.random())-2.5
             positions[i3+2] = (Math.random())
+
+            colors[i3+0] = 255
+            colors[i3+1] = 0
+            colors[i3+2] = 0 
         }else if(i<31500){
             positions[i3+0] = (Math.random())+16.5
             positions[i3+1] = (Math.random()*2.5)-1.5
             positions[i3+2] = (Math.random())
+
+            colors[i3+0] = 255
+            colors[i3+1] = 0
+            colors[i3+2] = 0 
         }else if(i<32000){
             positions[i3+0] = (Math.random())+17.5
             positions[i3+1] = (Math.random())
             positions[i3+2] = (Math.random())
+
+            colors[i3+0] = 255
+            colors[i3+1] = 0
+            colors[i3+2] = 0 
         }
 
     //s shape
@@ -240,25 +311,49 @@ for(let i = 0 ; i < count*3 ; i++ ){
         positions[i3+0] = (Math.random()*3.5)+21
         positions[i3+1] = (Math.random())
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 255
+        colors[i3+2] = 255 
     }else if(i<35500){
         positions[i3+0] = (Math.random())+21
         positions[i3+1] = (Math.random()*1.25)-1.25
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 255
+        colors[i3+2] = 255 
     }else if(i<37500){
         positions[i3+0] = (Math.random()*2.5)+22
         positions[i3+1] = (Math.random())-1.25
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 255
+        colors[i3+2] = 255 
     }else if(i<38500){
         positions[i3+0] = (Math.random())+23.5
         positions[i3+1] = (Math.random()*1.25)-2.45
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 255
+        colors[i3+2] = 255 
     }else if(i<39500){
         positions[i3+0] = (Math.random()*2.5)+21
         positions[i3+1] = (Math.random())-2.5
         positions[i3+2] = (Math.random())
+
+        colors[i3+0] = 255
+        colors[i3+1] = 255
+        colors[i3+2] = 255 
     }
 
-    colors[i] = Math.random()
+    // colors[i3+0] = 1
+    // colors[i3+1] = 1
+    // colors[i3+2] = 1
+    
+
     
 }
 
@@ -274,19 +369,26 @@ bitcoinGeometry.setAttribute(
     
 )
 
+bitcoinGeometry.setAttribute(
+    'rotation',
+     new THREE.BufferAttribute(rotation,3)    
+    
+)
+
+
 
 
 
 //texture
 const textureLoader = new THREE.TextureLoader()
-const particalTexture = textureLoader.load('img/ampersand.jpg')
+const particalTexture = textureLoader.load('img/ampersand.jpeg')
 
 //Material
 const bitcoinMaterial = new THREE.PointsMaterial();
-bitcoinMaterial.size = 0.09;
+bitcoinMaterial.size = 0.2;
 // bitcoinMaterial.sizeAttenuation = true;
 // bitcoinMaterial.color = new THREE.Color(0x5073f1)
-bitcoinMaterial.color = new THREE.Color("white")
+// bitcoinMaterial.color = new THREE.Color("red")
 // bitcoinMaterial.map = particalTexture
 bitcoinMaterial.transparent = true
 bitcoinMaterial.alphaMap = particalTexture
@@ -295,8 +397,9 @@ bitcoinMaterial.depthWrite = false
 bitcoinMaterial.alphaTest = 0.001
 
 // bitcoinMaterial.blending = THREE.AdditiveBlending
-// bitcoinMaterial.vertexColors = true
+bitcoinMaterial.vertexColors = true
 
+console.log(bitcoinGeometry,"bitcoinGeometry")
 //Partical
 const coinParticals = new THREE.Points(bitcoinGeometry,bitcoinMaterial);
 coinParticals.position.x = -14
@@ -304,6 +407,44 @@ group.add(coinParticals);
 
 
 scene.add(group)
+
+
+
+
+
+
+// // instantiate a loader
+// const loader = new THREE.ObjectLoader();
+
+// // load a resource
+// loader.load(
+// 	// resource URL
+// 	'img/3d/3d.obj',
+// 	// called when resource is loaded
+// 	function ( object ) {
+
+// 		scene.add( object );
+
+// 	},
+// 	// called when loading is in progresses
+// 	function ( xhr ) {
+
+// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+// 	},
+// 	// called when loading has errors
+// 	function ( error ) {
+
+// 		console.log( 'An error happened' );
+
+// 	}
+// );
+
+
+
+
+
+
 
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
@@ -343,7 +484,7 @@ function render() {
 
     const time = Date.now() * 0.00005;
 
-    camera.position.x = ( mouseX - camera.position.x ) * 0.0005;
+    camera.position.x = ( mouseX - camera.position.x ) * 0.0008;
     camera.position.y = ( - mouseY - camera.position.y ) * 0.001;
 
     camera.lookAt( scene.position );
@@ -420,8 +561,8 @@ genrateLogoLine();
 // )
 // scene.add(cube);
 
-const light = new THREE.PointLight( "white", 5, 100 );
-light.position.set( 50, 50, 50 );
+const light = new THREE.PointLight( "white", 5, 107 );
+light.position.set( 50, 60, 50 );
 scene.add( light );
 
 camera.position.z = 13;
